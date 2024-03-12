932ee4300e23043048d23ab0ddeaf30137135865
Etienne Binginot <etienne.binginot@gmail.com>
2024-03-12 19:54:42 +0100

    feature: configure tailwindcss

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./templates/**/*.{html,js}",
+      "./assets/**/*.js",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
