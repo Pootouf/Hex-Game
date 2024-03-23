8e8aa9f425236ae53704ddc18123607467681530
Etienne Binginot <etienne.binginot@gmail.com>
2024-03-23 18:43:53 +0100

    fix: use jinja extension for template files

--- app/src/blueprint/HexBlueprint.py
+++ app/src/blueprint/HexBlueprint.py
@@ -3,4 +3,4 @@ from flask import Blueprint, render_template
 hex_route = Blueprint('hex_route', __name__, template_folder='templates')
 @hex_route.route('/')
 def show():
-    return render_template('index.html')
+    return render_template('index.html.jinja')
