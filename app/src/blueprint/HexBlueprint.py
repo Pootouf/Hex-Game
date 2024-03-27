e064055db71429a19002a0a4ecea0e2898f9f176
ellexart <axelle.mazuy@gmail.com>
2024-03-27 11:26:23 +0100

    feature: add buttons to navigate to game parameters

--- app/src/blueprint/HexBlueprint.py
+++ app/src/blueprint/HexBlueprint.py
@@ -4,3 +4,7 @@ hex_route = Blueprint('hex_route', __name__, template_folder='templates')
 @hex_route.route('/')
 def show():
     return render_template('index.html.jinja')
+
+@hex_route.route('/hex/game/parameters')
+def selectGameParametersGame():
+    return render_template('game/gamePage.html.jinja')
