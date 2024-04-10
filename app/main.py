f9447310247f0a5480d0420b40e30edd2e757451
ellexart <axelle.mazuy@gmail.com>
2024-04-10 16:36:17 +0200

    feature: add route for redirecting to the selection of game parameters, management of game parameters

--- app/main.py
+++ app/main.py
@@ -2,5 +2,6 @@ from flask import Flask
 from src.blueprint.HexBlueprint import hex_route
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abc'
 app.register_blueprint(hex_route)
 
--- app/src/blueprint/HexBlueprint.py
+++ app/src/blueprint/HexBlueprint.py
@@ -1,17 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, request
 
 from src.service.HexService import createGame, createHeuristicTree, applyMinimax, applyNegamax, applyAlphaBeta
 
 hex_route = Blueprint('hex_route', __name__, template_folder='templates')
-
 
 @hex_route.route('/')
 def show():
     return render_template('index.html.jinja')
 
 
-@hex_route.route('/hex/game/parameters')
-def selectGameParametersGame():
+@hex_route.route('/hex/game/create/game/example')
+def createExample():
     game = createGame(4, 1)
     heuristictree = createHeuristicTree(game.board, 4)
     applyMinimax(heuristictree)
@@ -22,3 +21,15 @@ def selectGameParametersGame():
     heuristictree = createHeuristicTree(game.board, 4)
     applyAlphaBeta(heuristictree)
     return render_template('game/gamePage.html.jinja', game=game, heuristictree=heuristictree)
+
+@hex_route.route('/hex/game/create/game/parameters')
+def createGameParameters():
+    return render_template('game/gamePage.html.jinja')
+
+@hex_route.route('/hex/game/parameters')
+def selectGameParameters():
+    game = createGame(int(request.args['board-size']), int(request.args['difficulty']))
+    heuristictree = createHeuristicTree(game.board, 1) #TODO height
+    session['board-size'] = request.args['board-size']
+    session['difficulty'] = request.args['difficulty']
+    return render_template('game/gameBoard.html.jinja')
