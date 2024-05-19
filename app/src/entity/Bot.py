21324dc86f87ef0116ac5ee7a3fdc8e6c7ac3c8b
ellexart <axelle.mazuy@gmail.com>
2024-05-19 15:18:44 +0200

    refactor: remove unused method in Bot, add attribute and getColor method for Bot

--- app/src/entity/Bot.py
+++ app/src/entity/Bot.py
@@ -3,15 +3,10 @@ from src.entity.Player import Player
 
 
 class Bot(Player):
-    nextMove : {int, int}
+    color:str
 
     def __init__(self, color):
         super().__init__(color)
 
-
-    def getNextMove(self) -> {int, int}:
-        return self.nextMove
-
-    def calculateNextMove(self, board:Board, difficultyLevel:int):
-        #   TODO : method
-        self.nextMove = self.getNextMove()
+    def getColor(self) -> str:
+        return self.color
